f85da2ddc2b3e9b81b9382f030c65e3d35941997
Benjamin Cardenas <bencarsal@gmail.com>
2017-02-03 15:04:55 -0300

    Update for compatibility with jruby or rails 5
    This each_with_index not recognizable for jruby or rails 5. Change for each.with_index

--- test/drawing/tc_named_axis_data.rb
+++ test/drawing/tc_named_axis_data.rb
@@ -18,7 +18,7 @@
 #     doc = Nokogiri::XML(@chart.to_xml_string)
 #     assert_equal(doc.xpath("//c:val/c:numRef/c:f").size,1)
 #     assert_equal(doc.xpath("//c:numCache/c:ptCount[@val='#{@series.data.size}']").size,1)
-#     @series.data.each_with_index do |datum, index|
+#     @series.data.each.with_index do |datum, index|
 #       assert_equal(doc.xpath("//c:numCache/c:pt[@idx='#{index}']").size,1)
 #       assert_equal(doc.xpath("//c:numCache/c:pt/c:v[text()='#{datum}']").size,1)
 #     end
